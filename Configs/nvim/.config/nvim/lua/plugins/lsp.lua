e042436e7d71d4a275516a0b0c6c8587c0e536af
Vitor Soares Silva <silva.vitor.soares@gmail.com>
2024-05-04 19:18:00 -0300

    add debug plugins to nvim

--- Configs/nvim/.config/nvim/lua/plugins/lsp.lua
+++ Configs/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -91,15 +91,25 @@ return {
 							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
 						})
-					end
-				end,
-			})
 
-			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-				callback = function(event)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event.buf })
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+
+						-- The following autocommand is used to enable inlay hints in your
+						-- code, if the language server you are using supports them
+						--
+						-- This may be unwanted, since they displace some of your code
+						if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+							map("<leader>th", function()
+								vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+							end, "[T]oggle Inlay [H]ints")
+						end
+					end
 				end,
 			})
 
