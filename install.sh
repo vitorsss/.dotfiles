b1bfacd079299fd1b994f59d47bc330a0aaed004
Vitor Soares Silva <silva.vitor.soares@gmail.com>
2025-01-10 11:30:02 -0300

    add ghostty

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ source $DOTFILES/config.sh
 
 case $OS in
     macos) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-           brew install gcc make unzip zip git ripgrep tmux jq btop zsh rust fd python3 platformio ncspot;
+           brew install gcc make unzip zip git ripgrep tmux jq btop zsh rust fd python3 platformio ncspot ghostty;
     ;;
     ubuntu|debian) sudo apt update;
-                   sudo apt install -y gcc make unzip zip git ripgrep tmux curl jq btop zsh rust-all fd-find python3 platformio ncspot;
+                   sudo apt install -y gcc make unzip zip git ripgrep tmux curl jq btop zsh rust-all fd-find python3 platformio ncspot ghostty;
     ;;
     arch) sudo pacman --sync --refresh;
-          sudo pacman --sync --needed --noconfirm gcc make unzip zip git ripgrep tmux curl jq btop zsh rust fd python3 platformio-core ncspot;
+          sudo pacman --sync --needed --noconfirm gcc make unzip zip git ripgrep tmux curl jq btop zsh rust fd python3 platformio-core ncspot ghostty;
     ;;
 esac
 
